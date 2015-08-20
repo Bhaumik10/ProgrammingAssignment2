591afbe39062525fe93217f07fc715650500b282
Bhaumik10 <bp1232@nyu.edu>
2015-08-19 23:19:56 -0400

    add function

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,17 @@
 ## functions do
 
 ## Write a short comment describing this function
-#harsh
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(x = matrix()) {
+ m_inverse <- NULL      #Inititalize with NULL
+  set <- function(y) {
+    x <<- y
+    m_inverse <<- NULL   #Inititalize with NULL
+  }
+  get <- function() x
+  set_mat_inverse <- function(inverse) m_inverse <<- inverse
+  get_mat_inverse <- function() m_inverse
+  list(set=set, get=get, set_mat_inverse=set_mat_inverse, get_mat_inverse=get_mat_inverse)
 }
 
 
